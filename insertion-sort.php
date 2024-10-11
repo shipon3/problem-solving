4e6addc8e2bd4d6fe356e8eeb23b0bd9e81dbf03
shipon3 <engshipon3@gmail.com>
2024-10-11 07:53:52 +0600

    insertion short algorithm test 3 loop like for with while for loop and foreach loop now working insertion short 3 loop

--- insertion-sort.php
+++ insertion-sort.php
@@ -28,6 +28,38 @@ function insertionFromBIgToSmall(array $array) : array
     return $array;
 }
 
-$result = insertionFromBIgToSmall([1,3,2,6,7,5]);
+function insertionSortForLoop(array $array) : array
+{
+    for($i = 1; $i < count($array); $i ++){
+        $temp = $array[$i];
+        $j = $i - 1;
+        for($j; $j >=0 && $array[$j] < $temp; $j --){
+            $array[$j + 1] = $array[$j];
+        }
+        $array[$j+1] = $temp;
+    }
+    return $array;
+}
+
+function insertionSortForeach(array $array) : array
+{
+    foreach($array as $key => $item){
+        $j = $key;
+        $temp_array = array_reverse(array_slice($array, 0, $key),true);
+        foreach($temp_array as $index => $inItem){
+            if($inItem > $item){
+                break;
+            }else{
+                $array[$index +1] = $inItem;
+                $j = $index;
+            }
+            
+        }
+        $array[$j] = $item;
+    }
+    return $array;
+}
+
+$result = insertionSortForeach([1,3,2,6,7,5]);
 
 print_r($result);
